25b8bb4c32a73c3f5c910a463d087a4b31579ed3
ienjir <info@ienjir.dev>
2026-01-14 10:03:08 +0100

    Added print bitboard and enum for pieces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
+    print_bitboard(0);
 }
+
+fn print_bitboard(bitboard: u64) {
+    const LAST_BIT: u64 = 63;
+    for rank in 0..8 {
+        for file in (0..8).rev() {
+            let mask = 1u64 << (LAST_BIT - (rank * 8) - file);
+            let char = if bitboard & mask != 0 { '1' } else { '0' };
+            print!("{char} ");
+        }
+        println!()
+    }
+}
+
+pub enum Pieces {
+    King = 0,
+    Queen = 1,
+    Rook = 2,
+    Bishop = 3,
+    Knight = 4,
+    Pawn = 5,
+}
+
