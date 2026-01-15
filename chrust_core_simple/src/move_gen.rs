b356ebc26353e5da60440b3af7bd6f850a896d8d
ienjir <info@ienjir.dev>
2026-01-15 11:31:45 +0100

    Added rook targets with no colision checking

--- chrust_core_simple/src/move_gen.rs
+++ chrust_core_simple/src/move_gen.rs
@@ -3,6 +3,7 @@ use crate::{Square, file, rank};
 const KNIGHT_OFFSET: [i8; 8] = [-17, -15, -10, -6, 6, 10, 15, 17];
 const KING_OFFSET: [i8; 8] = [-9, -8, -7, -1, 1, 7, 8, 9];
 
+// For emtpy board
 pub fn king_targets(current_square: Square) -> Vec<Square> {
     let mut target_squares = Vec::new();
 
@@ -28,6 +29,7 @@ pub fn king_targets(current_square: Square) -> Vec<Square> {
     target_squares
 }
 
+// For emtpy board
 pub fn knight_targets(current_square: Square) -> Vec<Square> {
     let mut target_squares = Vec::new();
     
@@ -47,6 +49,43 @@ pub fn knight_targets(current_square: Square) -> Vec<Square> {
 
         if (file_difference == 2 && rank_difference == 1) || (file_difference == 1 && rank_difference == 2) {
             target_squares.push(candidate_square_u);
+        }
+    }
+
+    target_squares
+}
+
+// For emtpy board
+pub fn rook_targets(inital_square: Square) -> Vec<Square> {
+    let mut target_squares = Vec::new();
+
+    let directions: [i16; 4] = [-8, 8, -1, 1];
+    for direction_increment in directions {
+        let mut current_square: i16 = inital_square as i16;
+        loop {
+            let next = current_square + direction_increment;
+            
+            if !(0..=63).contains(&next) {
+                break;
+            }
+
+            let candidate_square_u = next as u8;
+            let file_difference = (file(candidate_square_u) as i8 - file(current_square as u8) as i8).abs(); 
+            let rank_difference = (rank(candidate_square_u) as i8 - rank(current_square as u8) as i8).abs();
+
+            if direction_increment == 8 || direction_increment == -8 { 
+                if file_difference != 0 || rank_difference != 1 {
+                    break;
+                }
+            } else { 
+                if rank_difference != 0 || file_difference != 1 {
+                    break;
+                }
+
+            }
+
+            target_squares.push(candidate_square_u);
+            current_square = next;
         }
     }
 
@@ -86,7 +125,7 @@ mod tests {
         let moves = king_targets(27);
 
         assert_eq!(moves.len(), 8);
-    
+
         let expected = [18, 19, 20, 26, 28, 34, 35, 36];
         for square in expected {
             assert!(moves.contains(&square));
@@ -98,7 +137,7 @@ mod tests {
         let moves = king_targets(63);
 
         assert_eq!(moves.len(), 3);
-    
+
         let expected = [62, 54, 55];
         for square in expected {
             assert!(moves.contains(&square));
--- chrust_ui_simple/src/main.rs
+++ chrust_ui_simple/src/main.rs
@@ -1,12 +1,12 @@
-use chrust_core_simple::{Square, move_gen::knight_targets};
+use chrust_core_simple::{Square, move_gen::{knight_targets, rook_targets}};
 
 fn main() {
-   let test_square: Square = 50;
+   let test_square: Square = 63;
     
-   let knight_squares = knight_targets(test_square);
+   let rook_squares = rook_targets(test_square);
 
-   for knight_square in knight_squares {
-        println!("Square: {knight_square}")
+   for rook_square in rook_squares {
+        println!("Square: {rook_square}")
    }
     
 }
