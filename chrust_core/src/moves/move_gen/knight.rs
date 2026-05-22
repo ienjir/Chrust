abdc13b9d901340e20b0b930088c21e3e033c400
ienjir <info@ienjir.dev>
2026-05-22 23:35:08 +0200

    Used attack tables in knight

--- chrust_core/src/moves/move_gen/knight.rs
+++ chrust_core/src/moves/move_gen/knight.rs
@@ -1,5 +1,6 @@
 use crate::{
 	ColoredPiece, Square,
+	attack_tables::KNIGHT_TARGETS,
 	errors::ChessError,
 	helper::{file_diff, in_bounds, rank_diff},
 	moves::make_move::{Move, MoveKind},
@@ -10,41 +11,27 @@ impl Position {
 	pub(crate) fn knight_targets(&self, knight: ColoredPiece, from_square: Square) -> Result<Vec<Move>, ChessError> {
 		let mut target_moves: Vec<Move> = Vec::with_capacity(8);
 
-		let directions: [i16; 8] = [-17, -15, -10, -6, 6, 10, 15, 17];
-
-		for direction in directions {
-			let candidate_square_i = from_square as i16 + direction;
-
-			if !in_bounds(candidate_square_i) {
-				continue;
-			}
-
-			if !matches!((file_diff(candidate_square_i, from_square), rank_diff(candidate_square_i, from_square)), (2, 1) | (1, 2)) {
-				continue;
-			}
-
-			match self.board[candidate_square_i as usize] {
+		for attack_square in &KNIGHT_TARGETS.get().unwrap()[from_square as usize] {
+			match self.board[*attack_square as usize] {
 				None => {
 					target_moves.push(Move {
 						colored_piece: knight,
 						from_square: from_square,
-						to_square: candidate_square_i as u8,
+						to_square: *attack_square,
 						move_kind: MoveKind::Quiet,
 					});
 				}
 				Some(colored_piece) => {
-					if colored_piece.side == knight.side {
-						continue;
+					if colored_piece.side != knight.side {
+						target_moves.push(Move {
+							colored_piece: knight,
+							from_square: from_square,
+							to_square: *attack_square,
+							move_kind: MoveKind::Capture,
+						});
 					}
-
-					target_moves.push(Move {
-						colored_piece: knight,
-						from_square: from_square,
-						to_square: candidate_square_i as u8,
-						move_kind: MoveKind::Capture,
-					});
 				}
-			};
+			}
 		}
 
 		Ok(target_moves)
